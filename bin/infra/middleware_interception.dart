6459021c7e19b62f3a0848679cc771e2da9c3ffc
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-08-21 16:24:21 -0300

    adiciona middleware: json  nas responses

---
+++ bin/infra/middleware_interception.dart
@@ -0,0 +1,8 @@
+import 'package:shelf/shelf.dart';
+
+class MiddlewareInterception {
+  Middleware get middleware => createMiddleware(
+        responseHandler: (Response res) =>
+            res.change(headers: {'content-type': 'application/json'}),
+      );
+}
